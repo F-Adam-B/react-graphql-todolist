53785c23220704adb6b14481e50bf59eaaff8641
adam <f.adambrad@gmail.com>
2018-05-01 12:29:35 -0600

    fully functioning app

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/style.css";
+import UserListContainer from "./containers/UserListContainer";
+import TodoListContainer from "./containers/TodoListContainer";
+import { Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={UserListContainer} />
+      <Route path="/todos/:userId" component={TodoListContainer} />
+    </Switch>
+  );
+};
 
 export default App;
