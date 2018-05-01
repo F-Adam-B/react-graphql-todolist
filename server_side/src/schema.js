e40df3fcf37d07e82e679cc00934342df0cda9e8
Adam Bradbury <f.adambrad@gmail.com>
2018-04-30 22:00:11 -0600

    initialized express server and graphql endpoint

---
+++ server_side/src/schema.js
@@ -0,0 +1,5 @@
+const Users = require('./data/users');
+const Todos = require('./data/todos');
+const find = require('lodash/find');
+const filter = require('lodash/filter');
+const sumBy = require('lodash/sumBy');
